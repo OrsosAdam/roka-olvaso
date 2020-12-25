4e043b4f3b4253d35808eb50da21c08bc2f2b82a
Orsós Ádám <adam@orsos.dev>
2020-12-25 03:00:19 +0100

    RO-2 Jobb typografia tailwind typography plugin hasznalataval

--- content.js
+++ content.js
@@ -1,24 +1,50 @@
-const links = document.querySelectorAll('a');
+function addStyles(styles) {
+    const head = document.querySelector('head');
+    for (const style of styles) {
+        const styleTag = document.createElement('link');
+        styleTag.rel = "stylesheet";
+        styleTag.href = style;
+        head.appendChild(styleTag);
+    }
+}
+
+function grabLinks() {
+    const links = document.querySelectorAll('a');
+    navigation.classList.add('navigation');
+    for (let i = 0; i < links.length; i++) {
+        links[i].classList.add('link');
+        links[i].classList.add('hover:text-black');
+        navigation.appendChild(links[i]);
+    }
+}
+function grabText() {
+    const texts = document.querySelectorAll('p');
+    for (let i = 0; i < texts.length; i++) {
+        text.appendChild(texts[i]);
+    }
+}
 const header = document.querySelector('h1');
-const texts = document.querySelectorAll('p');
 const body = document.querySelector('body');
-body.innerHTML = '';
-header.classList.add('heading')
-body.appendChild(header);
-const container = document.createElement('div');
-container.classList.add('container');
 const navigation = document.createElement('div');
-navigation.classList.add('navigation');
-container.appendChild(navigation);
-const text = document.createElement('div');
-text.classList.add('text');
-for(let i = 0; i < links.length;i++){
-    links[i].classList.add('link')
-    navigation.appendChild(links[i]);
+const container = document.createElement('div');
+const text = document.createElement('article');
+function createSite() {
+    text.classList.add('prose');
+    const customStyles = [
+        'https://unpkg.com/tailwindcss@^1.5/dist/base.min.css',
+        'https://unpkg.com/tailwindcss@^1.5/dist/components.min.css',
+        'https://unpkg.com/@tailwindcss/typography@0.2.x/dist/typography.min.css',
+        'https://unpkg.com/tailwindcss@^1.5/dist/utilities.min.css'
+    ];
+    addStyles(customStyles);
+    grabLinks();
+    grabText();
+    container.classList.add('container');
+    container.appendChild(navigation);
+    body.innerHTML = '';
+    header.classList.add('heading')
+    body.appendChild(header);
+    container.appendChild(text);
+    body.appendChild(container);
 }
-for(let i = 0; i < texts.length;i++){
-    texts[i].classList.add('paragraph')
-    text.appendChild(texts[i]);
-}
-container.appendChild(text);
-body.appendChild(container);
+createSite();
